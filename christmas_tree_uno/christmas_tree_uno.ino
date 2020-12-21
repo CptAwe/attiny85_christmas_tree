1866226ebefc73e0adbfffdad3bf590de7f76d5f
CptAwe <georgepapoulias@gmail.com>
2020-12-21 12:06:06 +0000

    Made it blink in sync with a song

--- christmas_tree_uno/christmas_tree_uno.ino
+++ christmas_tree_uno/christmas_tree_uno.ino
@@ -4,69 +4,68 @@
 */ 
 #include "LED.cpp"
 
-#define red_LEDs_pin     3
+#define red_LEDs_pin     5
 #define yellow_LEDs_pin  6
-#define blue_LEDs_pin    5
-#define flash_duration 500
+#define blue_LEDs_pin    3
 
-byte led;
-int previous_choice = 0;
-
-LED red(red_LEDs_pin, 100);
+LED red(red_LEDs_pin, 100, 1000, 100);
 LED yellow(yellow_LEDs_pin, 200);
 LED blue(blue_LEDs_pin, 15);
 
+int pause_duration = 300;
+int smaller_pause_duration = 70;
+int pause_after_each_note = 360;
+
+// This represents the notes for Stand by Me - Ben E. King
+char red_notes[]    = "._._-----------------------------..._._..._._";
+char blue_notes[]   = "----..._._-----------------..._._------------";
+char yellow_notes[] = "----------..._._.._._..._._------------------";
+byte num_of_notes = sizeof(red_notes)/sizeof(char);
+
 void setup() {
-	randomSeed(analogRead(0));
+	// nothing to do here
 }
 
 void loop() {
+	for (byte note=0; note < num_of_notes; note++){
 
-	while (true) {
-		// Avoid doing the same thing twice
-		led = random(7);
-		if (led != previous_choice) {
-			break;
+		if (red_notes[note] == '.') {
+			red.on();
+		} else if (red_notes[note] == '_') {
+			delay(pause_duration);
+			continue;
+		} else if (red_notes[note] == '~') {
+			delay(smaller_pause_duration);
+			continue;
 		}
+
+		if (blue_notes[note] == '.') {
+			blue.on();
+		} else if (blue_notes[note] == '_') {
+			delay(pause_duration);
+			continue;
+		} else if (red_notes[note] == '~') {
+			delay(smaller_pause_duration);
+			continue;
+		}
+
+		if (yellow_notes[note] == '.') {
+			yellow.on();
+		} else if (yellow_notes[note] == '_') {
+			delay(pause_duration);
+			continue;
+		} else if (red_notes[note] == '~') {
+			delay(smaller_pause_duration);
+			continue;
+		}
+
+		delay(pause_after_each_note/2);
+
+		red.off();
+		blue.off();
+		yellow.off();
+
+		delay(pause_after_each_note/2);
+
 	}
-	switch (led) {
-		case 0:
-			red.flash();
-			break;
-		case 1:
-			yellow.flash();
-			break;
-		case 2:
-			blue.flash();
-			break;
-		case 3:
-			red.on();
-			yellow.on();
-			delay(flash_duration);
-			red.off();
-			yellow.off();
-		case 4:
-			yellow.on();
-			blue.on();
-			delay(flash_duration);
-			yellow.off();
-			blue.off();
-		case 5:
-			red.on();
-			blue.on();
-			delay(flash_duration);
-			red.off();
-			blue.off();
-			break;
-		default:
-			red.on();
-			yellow.on();
-			blue.on();
-			delay(flash_duration);
-			red.off();
-			yellow.off();
-			blue.off();
-			break;
-	}
-	previous_choice = led;
 }
